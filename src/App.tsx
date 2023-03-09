89b2d769e50c0c2ee2df77916e88fbf1d2d9e966
曾经沧海 <2426502977@qq.com>
2023-03-09 21:06:15 +0800

    函数数组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Robot from './components/robot/Robot'
-import robots from './mockdata/robots.json'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 import ShoppingCart from './components/shopping_cart/ShoppingCart'
@@ -12,48 +11,31 @@ interface State {
   count: number
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robotGallery: [],
-      count: 0
-    }
-  }
+const App: React.FC = (props) => {
+  const [count, setCount] = useState<number>(0)
 
-  componentDidMount(): void {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
 
-  render(): React.ReactNode {
-    return (
-      <div className={styles.App} >
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>罗伯特机器人炫酷吊炸天</h1>
-        </div>
-        <button
-          onClick={() => {
-            this.setState((preState, preProps) => { return { count: preState.count + 1 } }, () => {
-              console.log('count', this.state.count)
-            })
-            this.setState((preState, preProps) => { return { count: preState.count + 1 } }, () => {
-              console.log('count', this.state.count)
-            })
-          }}
-        >
-          Click
-        </button>
-        <span>count: {this.state.count}</span>
-        <ShoppingCart />
-        <div className={styles.robotList}>
-          {this.state.robotGallery.map(r => < Robot id={r.id} email={r.email} name={r.name} key={r.id} />)}
-        </div>
+  return (
+    <div className={styles.App} >
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天</h1>
       </div>
-    )
-  }
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Click
+      </button>
+      <span>count: {count}</span>
+      <ShoppingCart />
+      <div className={styles.robotList}>
+        {/* {this.state.robotGallery.map(r => <Robot id={r.id} email={r.email} name={r.name} key={r.id} />)} */}
+      </div>
+    </div>
+  )
+
 
 }
 
